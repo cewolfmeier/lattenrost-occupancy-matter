b71cc0d4370ed6a4069a456d01fd567fa43b609b
Cedric Wolfmeier <641_000004702@tln.bfw-hamburg.de>
2026-02-04 19:43:01 +0100

    add basic matter connection code

--- Lattenrost-Presence-Matter.ino
+++ Lattenrost-Presence-Matter.ino
@@ -1,7 +1,72 @@
-void setup() {
+#include <Matter.h>
 
+void setup()
+{
+  Serial.begin(115200);
+  Matter.begin();
+
+  pinMode(BTN_BUILTIN, INPUT_PULLUP);
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDR, HIGH);
+
+  Serial.println("Matter temperature sensor");
+
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("Matter device is not commissioned");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("QR code URL: %s\n", Matter.getOnboardingQRCodeUrl().c_str());
+  }
+  while (!Matter.isDeviceCommissioned()) {
+    delay(200);
+    decommission_handler();  // if the user button is pressed for 10 seconds
+  }
+
+  Serial.println("Waiting for Thread network...");
+  while (!Matter.isDeviceThreadConnected()) {
+    delay(200);
+    decommission_handler();
+  }
+  Serial.println("Connected to Thread network");
+
+  Serial.println("Waiting for Matter device discovery...");
+
+  Serial.println("Matter device is now online");
 }
 
-void loop() {
+void loop()
+{
+  decommission_handler();  // if the user button is pressed for 10 seconds
+  delay(2000);
+}
 
+void decommission_handler() {
+  if (digitalRead(BTN_BUILTIN) == LOW) {  //Push button pressed
+    // measures time pressed
+    int startTime = millis();
+    while (digitalRead(BTN_BUILTIN) == LOW) {
+      //delay(50);
+
+      int elapsedTime = (millis() - startTime) / 1000.0;
+
+      if (elapsedTime > 10) {
+        Serial.printf("Decommissioning!\n");
+        for (int i = 0; i < 10; i++) {
+          digitalWrite(LEDR, !(digitalRead(LEDR)));
+          delay(100);
+        };
+
+        if (!Matter.isDeviceCommissioned()) {
+          Serial.println("Decommission done!");
+          digitalWrite(LEDR, LOW);
+        } else {
+          Serial.println("Matter device is commissioned-> Starting Decommission process");
+          nvm3_eraseAll(nvm3_defaultHandle);  // Decomission command
+          digitalWrite(LED_BUILTIN, LOW);
+          Serial.println("Decommission done!");
+        }
+        break;
+      }
+    }
+  }
 }
