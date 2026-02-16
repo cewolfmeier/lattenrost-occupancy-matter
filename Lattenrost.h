28e6e18c0fa371e20aa9a35678eddb6978701b74
cewolfmeier <wolfmeier.cedric@gmail.com>
2026-02-16 18:07:45 +0100

    add bubblesort and median to filter out noise

--- Lattenrost.h
+++ Lattenrost.h
@@ -30,7 +30,55 @@ public:
     return g.gyro.z * 180.0 / M_PI;  // rad/s -> deg/s
   }
 
+  float calculate_median(float values[], bool filter_outliers=false){
+    int start_value = 0;
+    int stop_value = sizeof(values);
+    int calc_len = sizeof(values);
+    int sum = 0;
+    if (filter_outliers) {
+      bubble_sort(values);
+      start_value++;
+      stop_value--;
+      calc_len=-2;
+    }
+    for (int i=start_value; i<stop_value; i++){
+      sum=+values[i];
+    }
+    return sum/calc_len;
+
+  }
+
+  void bubble_sort(float arr[]){
+    int n = sizeof(arr);
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                float temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+      
+        // If no two elements were swapped, then break
+        if (!swapped)
+            break;
+    }
+  }
+
   bool is_occupied(float threshold, bool use_roll=false) {
+    float values[12];
+    float result = 0;
+    for (int i=0; i<sizeof(values); i++){
+      values[i] = use_roll ? get_roll() : get_pitch();
+    }
+    result = calculate_median(values, true);
+    result = result < 0 ? result*-1 : result;
+    return result > threshold;
+    /*
     if (use_roll) {
       float roll = get_roll();
       roll = roll < 0 ? roll*-1 : roll;
@@ -39,6 +87,7 @@ public:
     float pitch = get_pitch();
     pitch = pitch < 0 ? pitch*-1 : pitch;
     return pitch > threshold;
+    */
   }
 };
 
